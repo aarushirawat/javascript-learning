5313415b57a7ff16083edc0e0a1248c431e5bba1
aarushirawat <aarushirawat43@gmail.com>
2025-01-27 18:39:14 +0000

    container config done

---
+++ basics/02.js
@@ -0,0 +1,24 @@
+"use strict";//treat all JS code as newer version
+//alert("hello")//we are using node js and not browser
+
+//code readability should be high
+//official documentation =>tc39.es
+//mdn
+let name="aarushi"
+let age=34
+let isLoggedIn=false
+//number =>2 to power 53(range)
+//bigint
+// string (single quote or double quote)
+//boolean=>true/false
+//null =>standalone value
+//undefined =>variable is declared but not defined
+//null is represented of empty value
+let state=null
+//symbol =>used for uniqueness
+
+//object 
+console.log(typeof"aarushi");
+console.log(typeof age);//number
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined is a type 
---
+++ basics/variables.js
@@ -0,0 +1,19 @@
+const accountId=14453
+let accountEmail="aarushi@google.com"//can be changed
+var accountPassword="12345"
+accountCity="Dehradun"//dont use this
+// const means we cant change its value
+//accountId=2 //not allowed (cant change its value)
+console.log(accountId);
+// just type log and enter for shortcut
+accountEmail="hab@hfm.com"
+accountPassword="54324"
+accountCity="mumbai"
+let accountState;//undefined
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+//represents all data in tabular form
+//what is difference between let and var .
+// to declare variable ->var and let .nowadays let is used and not var
+/*
+prefer not to use var because of issue in block scope and functional scope.
+*/
