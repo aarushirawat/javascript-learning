379cf47eea9f16671e2509882f09fd5a44acbe78
aarushirawat <aarushirawat43@gmail.com>
2025-01-28 09:34:13 +0000

    operations

---
+++ basics/05_operations.js
@@ -0,0 +1,40 @@
+let value=3
+let negValue=-value
+console.log(negValue)//-3
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+
+let str="hello"
+let str2=" aarushi"
+let str3=str+str2
+
+console.log(str3)
+console.log("1"+2);//12
+console.log(1+"2");//12
+console.log(2%3);
+
+
+console.log("1"+2);//12
+console.log(1+"2");//12
+console.log("1"+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+console.log(true)
+console.log(+true)//1
+console.log(+"")//0
+let num1,num2,num3
+num1=num2=num3=2+2
+//readability should be good
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter)//101
+let gameCounter2=100
+++gameCounter2;
+console.log(gameCounter2)//101
+//js mdn
